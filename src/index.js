20623ecc77f359168b9e0a1700928eb78102d069
ABespolov <abespolov@gmail.com>
2018-03-06 17:21:44 +0300

    feature: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+    var result = [1];
+    array = array.sort((a, b) => a - b);
+
+
+    try {
+        array.reduce((prev, curr, index, arr) => {
+            if (curr - prev == 1) {
+                result[result.length - 1]++;
+            } else {
+                result.push(1);
+            }
+            return curr;
+        });
+    } catch (error) {
+        return 0;
+    }
+
+    return Math.max.apply(null, result);
+
 }
