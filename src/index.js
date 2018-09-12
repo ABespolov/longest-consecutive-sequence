8777c2b25092908172937518da20c6572fe862ea
Artem Bespolov <abespolov@gmail.com>
2018-09-12 09:14:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
+    var show = false;
 
-    var result = [1];
+    var result = [];
+
+
+    function uniq(a) {
+        var seen = {};
+        return a.filter(function(item) {
+            return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+        });
+    }
+    array = uniq(array);
     array = array.sort((a, b) => a - b);
 
-
     try {
+
         array.reduce((prev, curr, index, arr) => {
             if (curr - prev == 1) {
                 result[result.length - 1]++;
@@ -16,6 +26,8 @@ module.exports = function longestConsecutiveLength(array) {
     } catch (error) {
         return 0;
     }
+
+
 
     return Math.max.apply(null, result);
 
